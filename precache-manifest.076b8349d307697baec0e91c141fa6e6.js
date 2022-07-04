7cae80b15f95799a2f04764fafcb9d60c4898d25
Kalyan <jb97035@gmail.com>
2022-07-04 17:25:36 +0530

    Updates

---
+++ precache-manifest.076b8349d307697baec0e91c141fa6e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15d594368b56adc14aa8",
+    "url": "/vinewood/static/css/main.3a7b46f3.chunk.css"
+  },
+  {
+    "revision": "15d594368b56adc14aa8",
+    "url": "/vinewood/static/js/main.15d59436.chunk.js"
+  },
+  {
+    "revision": "8f0854ff6fd9d65f2f81",
+    "url": "/vinewood/static/js/1.8f0854ff.chunk.js"
+  },
+  {
+    "revision": "231c1deb9491b9aad0b6",
+    "url": "/vinewood/static/js/runtime~main.231c1deb.js"
+  },
+  {
+    "revision": "14eb14f786418c33c758b194ba482e4d",
+    "url": "/vinewood/index.html"
+  }
+];
